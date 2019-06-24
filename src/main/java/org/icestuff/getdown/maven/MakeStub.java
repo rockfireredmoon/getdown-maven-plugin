a9b72027e6f254410f22dc9b02dbe02964fe8e92
rockfireredmoon <rockfire.redmoon@gmail.com>
2019-06-24 09:43:55 +0100

    Messed up the merge a bit.

--- src/main/java/org/icestuff/getdown/maven/MakeStub.java
+++ src/main/java/org/icestuff/getdown/maven/MakeStub.java
@@ -47,7 +47,7 @@ public class MakeStub extends AbstractGetdownMojo {
 	protected void copyGetdownClient() throws MojoExecutionException {
 		getLog().info("Copying client jar");
 		Artifact getdown = (Artifact) plugin.getArtifactMap().get("com.threerings.getdown:getdown-launcher");
-		Util.copyFile(getdown.getFile(), new File(workDirectory, "getdown.jar"));
+		Util.copyFile(getdown.getFile(), new File(stubWorkDirectory, "getdown.jar"));
 	}
 
 	protected void makeConfigFile() throws FileNotFoundException {
